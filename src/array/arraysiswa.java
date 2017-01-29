a9bb63d2ce2cd3c5ea9c2e47a94786c045c4e814
nofitaaaa <nofitamahfudiyah@gmail.com>
2017-01-29 19:51:58 +0700

    initial commit

---
+++ src/array/arraysiswa.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author nofita
+ */
+public class arraysiswa 
+{
+    int nis [] [] = {{210651},{210651},{210652}};
+    String nama [] [] = {{"Harun Ulum"},{"Nur Azmi"},{"Devita Fahma"}};
+    
+    public void tampil(String n)
+    {
+        System.out.println("Identitas Siswa Angkatan 24");
+    }
+    
+    public void namanim (int n)
+    {
+        for (int i = 0 ; i < 1 ; i++)
+        {
+            for (int j = 0; j < 1; j++)
+            {
+                System.out.println(nama[2][j]+ " : " + nis [2][j]);
+            }
+        }
+    }
+    
+    public static void main (String[] args)
+    {
+        arraysiswa siswa = new arraysiswa();
+        siswa.tampil(null);
+        siswa.namanim(0);
+    }
+}
